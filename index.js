2f85689286b869d3e4d4f69431d63630003a4542
Igor Tkachuk <I.Tka4yk@gmail.com>
2025-09-19 23:09:16 +0300

    feat: add interactiv

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ const planetColorMap = [
 
 let currentPlanet = "Mercury";
 
+let data;
+
 window.addEventListener("load", main);
 
 function getPlanetColor(planetName) {
   return planetColorMap.find(
     ({ name }) => name.toUpperCase() === planetName.toUpperCase()
   ).color;
+}
+
+function getPlanetByName(planetName) {
+  return data.find(({name}) => planetName.toUpperCase() === name.toUpperCase())
 }
 
 async function getData() {
@@ -38,7 +44,7 @@ function makeNav(data) {
   data.forEach(({ name }) => {
     const li = document.createElement("li");
 
-    if (name === currentPlanet) {
+    if (name.toUpperCase() === currentPlanet.toUpperCase()) {
       li.style.borderTop = "4px solid var(--planet-color)";
       console.log("Pun`k!");
     }
@@ -57,6 +63,13 @@ function menuHandler({ target }) {
   if (!a) return;
 
   console.log(a.innerText);
+  currentPlanet = a.innerText;
+
+  const nav = document.querySelector("nav")
+  nav.innerHTML = ""
+  nav.appendChild(makeNav(data));
+
+  composePage(getPlanetByName(currentPlanet))
 }
 
 function btnSetHandler({ target }) {
@@ -73,10 +86,35 @@ function composePage({ name }) {
     "--planet-color",
     getPlanetColor(name)
   );
+
+  const planet = getPlanetByName(name);
+
+  document.querySelector("img.overview").setAttribute("src", planet.images.planet)
+  document.querySelector("img.structure").setAttribute("src", planet.images.internal)
+  document.querySelector("img.geology").setAttribute("src", planet.images.geology)
+
+  document.querySelector(".right-side h1").innerText = planet.name;
+  document.querySelector('.src a.overview').setAttribute('href', planet.overview.source);
+  document.querySelector(".descr.overview").innerText = planet.overview.content;
+  
+  document.querySelector('.src a.structure').setAttribute('href', planet.structure.source);
+  document.querySelector(".descr.structure").innerText = planet.structure.content;
+  
+  document
+    .querySelector('.src a.geology')
+    .setAttribute('href', planet.geology.source);
+  document.querySelector(".descr.geology").innerText = planet.geology.content;
+
+  document.querySelector("footer li:nth-child(1) h2").innerText = planet.rotation;
+  document.querySelector("footer li:nth-child(2) h2").innerText = planet.revolution;
+  document.querySelector("footer li:nth-child(3) h2").innerText = planet.radius;
+  document.querySelector("footer li:nth-child(4) h2").innerText = planet.temperature;
+
+
 }
 
 async function main() {
-  const data = await getData();
+  data = await getData();
 
   const nav = document.querySelector("nav");
   nav.appendChild(makeNav(data));
